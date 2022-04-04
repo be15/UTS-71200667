d592e1addd8f804faba97bb03c55b49bf022ba6f
be15 <79151758+be15@users.noreply.github.com>
2022-04-04 16:13:46 +0700

    Add files via upload

---
+++ BilanganPecahan/src/main/java/Fraction.java
@@ -0,0 +1,29 @@
+public class Fraction {
+
+    public Fraction(int, int){
+        return Fraction;
+    }
+
+    private int calculateDenominator(int,int,int);
+
+    private void checkMultiplesOfNumbers();
+
+    public Fraction division(Fraction, Fraction, Fraction){
+
+    }
+
+    public Fraction multiplication(Fraction, Fraction, Fraction){
+
+    }
+
+    public Fraction subtraction(Fraction, Fraction, Fraction){
+
+    }
+
+    public Fraction summation(Fraction, Fraction, Fraction){
+
+    }
+
+    public String toString(String){
+    }
+}
---
+++ BilanganPecahan/src/main/java/Mathematics.java
@@ -0,0 +1,48 @@
+public class Mathematics {
+    private int numerator;
+    private int denominator;
+
+    public Mathematics(int, int){
+        return Mathematics;
+    }
+
+    public Fraction division(Fraction, Fraction){
+        return division();
+    }
+
+    public int factorize(int, int){
+        return factorize();
+    }
+
+    public int getDenominator(){
+        return denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public Fraction multiplications(Fraction, Fraction){
+        return multiplications();
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public Fraction simplify(Fraction, Fraction){
+        return simplify();
+    }
+
+    public Fraction substaction(Fraction, Fraction){
+        return substaction();
+    }
+
+    public Fraction summation(Fraction, Fraction){
+        return summation();
+    }
+}
